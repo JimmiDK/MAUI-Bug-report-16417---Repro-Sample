57c310dbdd574c146a28e67bb7c52bb7f9c7fdcb
JimmiDK <jimmi.bendtson@gmail.com>
2023-08-01 10:59:10 +0200

    Initial commit

---
+++ MauiApp5/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrollView_OnScrolled(object sender, ScrolledEventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("ScrollView_OnScrolled: ScrollX:" + e.ScrollX + "ScrollY:" + e.ScrollY);
+        }
+    }
+}
